571e2df635ac523ac7b19d2db5277fe96b5d4bb6
Frans Billqvist <frans.billquist@gmail.com>
2022-10-13 16:13:30 +0200

    Added firebase to proj

--- src/Screens/StartScreen.tsx
+++ src/Screens/StartScreen.tsx
@@ -1,11 +1,19 @@
+import { addDoc, collection } from '@firebase/firestore';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import * as React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { fireStore } from '../Config/firebase';
 import { RootStackParamList } from '../Navigation/RootNavigator';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'StartScreen'>;
 
 export default function StartScreen({ navigation }: Props) {
+  const testfb = async () => {
+    await addDoc(collection(fireStore, 'test'), {name: 'test'})
+  };
+  const Testreg = async () => {
+    await addDoc(collection(fireStore, 'User'), {name: Date.now().toString()})
+  };
   return (
     <View style={styles.container}>
       <Text>Logga in eller skapa konto</Text>
@@ -13,6 +21,8 @@ export default function StartScreen({ navigation }: Props) {
       <Text>Knapp: Registrera konto</Text>
       <Button title='Log in' onPress={() => navigation.navigate('ManagerScreen')}></Button>
       <Button title='Register' onPress={() => navigation.navigate('RegisterScreen')}></Button>
+      <Button title='Register to DB' onPress={() => Testreg()}></Button>
+      <Button title='Test firebase add' onPress={() => testfb()}></Button>
     </View>
   );
 }
