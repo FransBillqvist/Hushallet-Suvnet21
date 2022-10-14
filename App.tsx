2b1a7705d43ffced60d9642ea3f1a1a46084b2b0
Daniel T <90195897+DTSD21@users.noreply.github.com>
2022-10-14 13:49:35 +0200

    add store, uuid, userslice

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider as ReduxProvider } from 'react-redux';
 import RootNavigator from './src/Navigation/RootNavigator';
+import { store } from './src/Store/store';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <StatusBar style='auto' />
-      <RootNavigator />
-    </SafeAreaProvider>
+    <ReduxProvider store={store}>
+      <SafeAreaProvider>
+        <StatusBar style='auto' />
+        <RootNavigator />
+      </SafeAreaProvider>
+    </ReduxProvider>
   );
 }
--- src/Screens/RegisterScreen.tsx
+++ src/Screens/RegisterScreen.tsx
@@ -1,17 +1,27 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { RootStackParamList } from '../Navigation/RootNavigator';
+import { useAppDispatch, useAppSelector } from '../Store/store';
+import { setUserName } from '../Store/userSlice';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'RegisterScreen'>;
 
 export default function RegisterScreen({ navigation }: Props) {
+  const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.user);
+
   return (
     <View style={styles.container}>
       <Text>Registrera användare</Text>
       <Text>Användarnamn</Text>
       <Text>Lösenord</Text>
       <Text>Lösenord igen</Text>
+      <Button
+        title='Test set name function'
+        onPress={() => dispatch(setUserName('Toasty McToaster'))}
+      />
+      <Text>Current state username: {user.username}</Text>
     </View>
   );
 }
