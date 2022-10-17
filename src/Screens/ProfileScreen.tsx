d92c27ab81296a9b166b076b12c230a77010e607
LuminTompa <lumincob@gmail.com>
2022-10-17 11:31:39 +0200

    be able to set profile name

--- src/Screens/ProfileScreen.tsx
+++ src/Screens/ProfileScreen.tsx
@@ -1,17 +1,40 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import SmallButton from '../Components/Buttons/SmallButton';
 import { RootStackParamList } from '../Navigation/RootNavigator';
+import { setProfileName } from '../Store/profileSlice';
+import { useAppDispatch } from '../Store/store';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'ProfileScreen'>;
 
 export default function ProfileScreen({ navigation }: Props) {
+  const dispatch = useAppDispatch();
+  const [name, setName] = React.useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Profile screen</Text>
-      <Text>Name</Text>
-      <Text>Avatar</Text>
-      <Text>Knapp: Skapa</Text>
+      <View style={styles.inputStyle}>
+        <Text style={styles.inputLabel}>Namn</Text>
+        <TextInput
+          style={styles.inputTextField}
+          placeholder='Profilnamn'
+          onChangeText={setName}
+          value={name}
+        />
+      </View>
+      <Text>Välj Avatar</Text>
+      <View>
+        <SmallButton
+          onPress={() => {
+            dispatch(setProfileName(name));
+          }}
+          startIcon={<MaterialIcons name='add-circle-outline' size={21} color='black' />}
+        >
+          Skapa
+        </SmallButton>
+      </View>
     </View>
   );
 }
@@ -20,7 +43,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+  },
+  inputLabel: {
+    marginBottom: 10,
+    fontSize: 15,
+  },
+  inputTextField: {
+    borderWidth: 1,
+    borderRadius: 7,
+    borderColor: 'darkgrey',
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    backgroundColor: 'white',
+    fontSize: 15,
+  },
+  inputStyle: {
+    marginTop: 10,
   },
 });
