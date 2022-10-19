676ba47ca7d30a6a83a58c58ec1a03bfbea2a0f8
Frans Billqvist <frans.billquist@gmail.com>
2022-10-19 11:02:06 +0200

    Renamed fireStore to db to easiler seperat

--- src/Config/firebase.ts
+++ src/Config/firebase.ts
@@ -17,8 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const fireStore = getFirestore(app);
+export const db = getFirestore(app);
 
-export { fireStore };
 
 export default app;
