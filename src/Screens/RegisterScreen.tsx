c50a453efa1056286077bd0e02262507e82fec27
LuminTompa <lumincob@gmail.com>
2022-10-19 15:09:43 +0200

    change all but view components to RNP components

--- src/Screens/RegisterScreen.tsx
+++ src/Screens/RegisterScreen.tsx
@@ -1,7 +1,8 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import * as React from 'react';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { View } from 'react-native';
+import { Button, Text, TextInput } from 'react-native-paper';
 import { RootStackParamList } from '../Navigation/RootNavigator';
 import { useAppDispatch, useAppSelector } from '../Store/store';
 import { setUserName, setUserPassword } from '../Store/userSlice';
@@ -15,37 +16,41 @@ export default function RegisterScreen({ navigation }: Props) {
   const [userName, onChangeUserName] = useState<string>('');
 
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Registrera användare</Text>
-
-      <TextInput
-        value={userName}
-        placeholder='fyll i användarnamn'
-        onChangeText={onChangeUserName}
-      ></TextInput>
-      <TextInput
-        value={password}
-        placeholder='fyll i lösenord'
-        onChangeText={onChangePassword}
-      ></TextInput>
-      <Button
-        title='Registrera användare'
-        onPress={() => {
-          dispatch(setUserName(userName));
-          dispatch(setUserPassword(password));
-        }}
-      />
-      <Text>Current user password: {user.password}</Text>
-      <Text>Current state username: {user.username}</Text>
+      <View>
+        <TextInput
+          value={userName}
+          placeholder='fyll i användarnamn'
+          onChangeText={onChangeUserName}
+        ></TextInput>
+        <TextInput
+          value={password}
+          placeholder='fyll i lösenord'
+          onChangeText={onChangePassword}
+        ></TextInput>
+      </View>
+      <View>
+        <Button
+          onPress={() => {
+            dispatch(setUserName(userName));
+            dispatch(setUserPassword(password));
+          }}
+        >
+          Registrera användare
+        </Button>
+        <Text>Current user password: {user.password}</Text>
+        <Text>Current state username: {user.username}</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
