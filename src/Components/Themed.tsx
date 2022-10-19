7ef347bff5b32cbee593c182a0bd7a45cf161931
LuminTompa <lumincob@gmail.com>
2022-10-19 21:32:19 +0200

    remove unused code

--- src/Components/Themed.tsx
+++ src/Components/Themed.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import {
-  Text as DefaultText,
-  TextInput as DefaultTextInput,
   View as DefaultView,
 } from 'react-native';
 import colors from '../colors';
@@ -25,29 +23,11 @@ type ThemeProps = {
   lightColor?: string;
   darkColor?: string;
 };
-
-export type TextProps = ThemeProps & DefaultText['props'];
 export type ViewProps = ThemeProps & DefaultView['props'];
-export type TextInputProps = ThemeProps & DefaultTextInput['props'];
-
-export function Text(props: TextProps) {
-  const { style, lightColor, darkColor, ...otherProps } = props;
-  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
-
-  return <DefaultText style={[{ color }, style]} {...otherProps} />;
-}
 
 export function View(props: ViewProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
 
   return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
-}
-
-export function TextInput(props: TextInputProps) {
-  const { style, lightColor, darkColor, ...otherProps } = props;
-  const backgroundColor = useThemeColor({ light: lightColor, dark: '#333333' }, 'background');
-  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
-
-  return <DefaultTextInput style={[{ backgroundColor, color }, style]} {...otherProps} />;
 }
