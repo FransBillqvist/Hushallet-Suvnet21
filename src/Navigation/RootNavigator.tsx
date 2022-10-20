3a4a76c66c73315ebab675c9c4e083758b6a1250
Frans Billqvist <frans.billquist@gmail.com>
2022-10-20 16:01:30 +0200

    Co-authored-by: Lucas Benjaminson tyvärr ;)

--- src/Navigation/RootNavigator.tsx
+++ src/Navigation/RootNavigator.tsx
@@ -13,6 +13,7 @@ import ProfileScreen from '../Screens/ProfileScreen';
 import RegisterScreen from '../Screens/RegisterScreen';
 import StartScreen from '../Screens/StartScreen';
 import StatisticsScreen from '../Screens/StatisticsScreen';
+// import { useAppSelector } from "../Store/store";
 
 declare global {
   namespace ReactNavigation {
@@ -36,20 +37,28 @@ export type RootStackParamList = {
 const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootNavigator({ colorScheme }: { colorScheme: ColorSchemeName }) {
+  // const user = useAppSelector((state) => state.user.user)
   return (
     <NavigationContainer theme={getTheme(colorScheme)}>
       <RootStack.Navigator>
         {/* DELETE DEVSTARTSCREEN BEFORE GOING LIVE!!*/}
-        <RootStack.Screen name='DevStartScreen' component={DevStartScreen} />
-        <RootStack.Screen name='StartScreen' component={StartScreen} />
-        <RootStack.Screen name='HomeScreen' component={HomeScreen} />
-        <RootStack.Screen name='ManagerScreen' component={ManagerScreen} />
-        <RootStack.Screen name='RegisterScreen' component={RegisterScreen} />
-        <RootStack.Screen name='ChoreScreen' component={ChoreScreen} />
-        <RootStack.Screen name='CreateScreen' component={CreateScreen} />
-        <RootStack.Screen name='StatisticsScreen' component={StatisticsScreen} />
-        <RootStack.Screen name='ProfileScreen' component={ProfileScreen} />
-        <RootStack.Screen name='DetailScreen' component={DetailScreen} />
+        {/* {!user ? ( */}
+          {/* <> */}
+          <RootStack.Screen name='DevStartScreen' component={DevStartScreen} />
+          <RootStack.Screen name='StartScreen' component={StartScreen} />
+          <RootStack.Screen name='RegisterScreen' component={RegisterScreen} />
+        {/* </> */}
+        {/* ) : ( */}
+          {/* <> */}
+          <RootStack.Screen name='HomeScreen' component={HomeScreen} />
+          <RootStack.Screen name='ManagerScreen' component={ManagerScreen} />
+          <RootStack.Screen name='ChoreScreen' component={ChoreScreen} />
+          <RootStack.Screen name='CreateScreen' component={CreateScreen} />
+          <RootStack.Screen name='StatisticsScreen' component={StatisticsScreen} />
+          <RootStack.Screen name='ProfileScreen' component={ProfileScreen} />
+          <RootStack.Screen name='DetailScreen' component={DetailScreen} />
+          {/* </> */}
+          {/* )} */}
       </RootStack.Navigator>
     </NavigationContainer>
   );
--- src/Screens/StartScreen.tsx
+++ src/Screens/StartScreen.tsx
@@ -1,29 +1,40 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import * as React from 'react';
+import { useState } from 'react'
 import { StyleSheet, View } from 'react-native';
 import { Text, TextInput } from 'react-native-paper';
 import BigButton from '../Components/Buttons/BigButton';
 import { RootStackParamList } from '../Navigation/RootNavigator';
+import { useAppDispatch, useAppSelector } from '../Store/store';
+import { login } from '../Store/userSlice'
+import { auth } from '../Config/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'StartScreen'>;
 
 export default function StartScreen({ navigation }: Props) {
+  const { isLoading, errorMsg} = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
         <View style={styles.inputStyle}>
           <Text style={styles.inputLabel}>Användarnamn</Text>
-          <TextInput style={styles.inputTextField} placeholder='Användarnamn'></TextInput>
+          <TextInput style={styles.inputTextField} value={email} onChangeText={setEmail} placeholder='Användarnamn'></TextInput>
         </View>
         <View style={styles.inputStyle}>
           <Text style={styles.inputLabel}>Lösenord</Text>
-          <TextInput style={styles.inputTextField} placeholder='Lösenord'></TextInput>
+          <TextInput style={styles.inputTextField} value={password} onChangeText={setPassword}  placeholder='Lösenord'></TextInput>
         </View>
       </View>
       <View style={styles.buttonContainer}>
-        <BigButton onPress={() => navigation.navigate('ManagerScreen')}>Logga In</BigButton>
+        <BigButton onPress={() => dispatch(login({email, password}))}>Logga In</BigButton>
         <Text style={styles.ellerText}>eller</Text>
         <BigButton onPress={() => navigation.navigate('RegisterScreen')}>Skapa konto</BigButton>
+        {errorMsg && <Text>{errorMsg}</Text>}
       </View>
     </View>
   );
