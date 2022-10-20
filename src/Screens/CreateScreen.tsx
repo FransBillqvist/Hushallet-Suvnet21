23fd9d6a4cd0e84526e0fed73e42acf76d955e61
Frans Billqvist <frans.billquist@gmail.com>
2022-10-20 10:22:48 +0200

    Rework git workflow

--- src/Screens/CreateScreen.tsx
+++ src/Screens/CreateScreen.tsx
@@ -22,8 +22,9 @@ export default function CreateScreen({ navigation }: Props) {
   if (text.length > 3) {
     code = nanoCode();
   }
+  //test a new line to merge
   const AddHouse = async () => {
-    await addDoc(collection(db, 'Household'), { id: nanoid(), name: text, code: code });
+    await addDoc(collection(db, 'Household'), { id: nanoId(), name: text, code: code });
   };
 
   const copyToClipboard = () => {
