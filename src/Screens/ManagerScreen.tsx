2d4d548d090aa724821abf3699d213324959597e
Frans Billqvist <frans.billquist@gmail.com>
2022-10-21 16:19:52 +0200

    Added join input/button checking if input == code

--- src/Screens/ManagerScreen.tsx
+++ src/Screens/ManagerScreen.tsx
@@ -1,18 +1,51 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Text, TextInput } from 'react-native-paper';
+import BigButton from '../Components/Buttons/BigButton';
+import { Household } from '../Data/household';
 import { RootStackParamList } from '../Navigation/RootNavigator';
+import { getHouseHoldByCode } from '../Store/householdSlice';
+import { useAppDispatch, useAppSelector } from '../Store/store';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'ManagerScreen'>;
 
-export default function ManagerScreen() {
+export default function ManagerScreen({ navigation }: Props) {
+  const [inviteCode, setInviteCode] = React.useState('');
+  const [isHide, setIsHide] = React.useState(true);
+  const buttonValue = isHide ? 'Gå med hushåll' : 'stäng';
+  const dispatch = useAppDispatch();
+  const households = useAppSelector((state) => state.household.households);
+
   return (
     <View style={styles.container}>
       <Text>Hushålls-skärm</Text>
       <Text>Knapp: Skapa nytt hushåll(ägare)</Text>
-      <Text>Dropdown menu: Gå med i ett hushåll</Text>
-      <Text>Knapp: Skriv in hushållskod(För att gå med i hushåll) Knapp: Acceptera</Text>
+      <BigButton onPress={() => setIsHide(!isHide)}>{buttonValue}</BigButton>
+      {isHide ? (
+        <></>
+      ) : (
+        <>
+          <TextInput
+            style={styles.input}
+            placeholder='kod'
+            value={inviteCode}
+            onChangeText={setInviteCode}
+          ></TextInput>
+          <BigButton
+            onPress={async () => {
+              const result = await dispatch(getHouseHoldByCode(inviteCode))
+                .unwrap();
+              console.log(result);
+              if(result)
+              {
+                navigation.navigate('ProfileScreen');
+              }
+              }}>
+            Skicka förfrågan
+          </BigButton>
+        </>
+      )}
       <Text>Lista med hushåll</Text>
     </View>
   );
@@ -23,5 +56,13 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
   },
 });
