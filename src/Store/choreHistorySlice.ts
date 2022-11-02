2efa2664cace7847b173df4702f9c8e49e70eefc
Daniel T <90195897+DTSD21@users.noreply.github.com>
2022-11-02 20:04:02 +0100

    remove unused code and old comments, add missing rejectvalues, clarify logs

--- src/Store/choreHistorySlice.ts
+++ src/Store/choreHistorySlice.ts
@@ -1,5 +1,6 @@
 import { addDoc, collection, getDocs, query, where } from '@firebase/firestore';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { FirebaseError } from 'firebase/app';
 import { filterCurrentWeek } from '../Components/filterChoreHistory';
 import { db } from '../Config/firebase';
 import { Chore } from '../Data/chore';
@@ -19,82 +20,70 @@ const initialState: ChoreHistoryState = {
   choresHistory: [],
 };
 
-export const addChoreHistoryToDb = createAsyncThunk<ChoreHistory, ChoreHistory>(
-  'household/addchorehistory',
-  async (choreHistory, thunkApi) => {
-    try {
-      console.log(choreHistory.date);
-      await addDoc(collection(db, 'ChoreHistory'), choreHistory);
-      return choreHistory;
-    } catch (error) {
-      return thunkApi.rejectWithValue(error);
+export const addChoreHistoryToDb = createAsyncThunk<
+  ChoreHistory,
+  ChoreHistory,
+  { rejectValue: string }
+>('household/addchorehistory', async (choreHistory, thunkApi) => {
+  try {
+    console.log(choreHistory.date);
+    await addDoc(collection(db, 'ChoreHistory'), choreHistory);
+    return choreHistory;
+  } catch (error) {
+    console.error(error);
+    if (error instanceof FirebaseError) {
+      console.log(error.message);
+      return thunkApi.rejectWithValue('Databasfel, kunde inte lägga till din syssla.');
     }
-  },
-);
+    return thunkApi.rejectWithValue('Något gick snett, vänligen kontakta support!');
+  }
+});
 
-export const getChoreHistoryFromDbByProfileIds = createAsyncThunk<ChoreHistory[], Profile[]>(
-  'household/getchorehistorybyprofileid',
-  async (profiles, thunkApi) => {
-    try {
-      if (profiles.length > 0) {
-        const profileids = profiles.map((pro) => pro.id);
-        const q = query(collection(db, 'ChoreHistory'), where('profileId', 'in', profileids));
-        const querySnapshot = await getDocs(q);
-        const result = querySnapshot.docs.map((doc) => doc.data() as ChoreHistory);
-        return result;
-      } else return [];
-    } catch (error) {
-      return thunkApi.rejectWithValue(error);
-    }
-  },
-);
-
-export const getChoreHistoryFromDbByChores = createAsyncThunk<ChoreHistory[], Chore[]>(
-  'household/getchorehistorybychoreid',
-  async (chores, thunkApi) => {
-    try {
-      if (chores.length > 0) {
-        const choreIds = chores.map((c) => c.id);
-        const q = query(collection(db, 'ChoreHistory'), where('choreId', 'in', choreIds));
-        const querySnapshot = await getDocs(q);
-        const result = querySnapshot.docs.map((doc) => doc.data() as ChoreHistory);
-        return result;
-      } else return [];
-    } catch (error) {
-      return thunkApi.rejectWithValue(error);
-    }
-  },
-);
-
-export const getDateWhenLatestDoneChoreHistoryWithChoreId = createAsyncThunk<ChoreHistory, string>(
-  'household/getlastestdatefromchorehistory',
-  async (choreId, thunkApi) => {
-    try {
-      let worstcase: ChoreHistory = {
-        id: '',
-        choreId: choreId,
-        profileId: '',
-        date: new Date(0).toISOString().slice(0, 10),
-      };
-      const historyRef = collection(db, 'ChoreHistory');
-      const q = query(historyRef, where('choreId', '==', choreId));
+export const getChoreHistoryFromDbByProfileIds = createAsyncThunk<
+  ChoreHistory[],
+  Profile[],
+  { rejectValue: string }
+>('household/getchorehistorybyprofileid', async (profiles, thunkApi) => {
+  try {
+    if (profiles.length > 0) {
+      const profileids = profiles.map((pro) => pro.id);
+      const q = query(collection(db, 'ChoreHistory'), where('profileId', 'in', profileids));
       const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((doc) => {
-        const loopdate = new Date(doc.data().date);
-        if (loopdate > new Date(worstcase.date)) {
-          worstcase = doc.data() as ChoreHistory;
-        }
-      });
-      const choreHistories: ChoreHistory = worstcase;
-      return choreHistories;
-    } catch (error) {
-      return thunkApi.rejectWithValue(error);
+      const result = querySnapshot.docs.map((doc) => doc.data() as ChoreHistory);
+      return result;
+    } else return [];
+  } catch (error) {
+    console.error(error);
+    if (error instanceof FirebaseError) {
+      console.log(error.message);
+      return thunkApi.rejectWithValue('Databasfel, kunde inte hämta historiken.');
     }
-  },
-);
+    return thunkApi.rejectWithValue('Något gick snett, vänligen kontakta support!');
+  }
+});
 
-// const findProfileId = querySnapshot.docs.map((doc) => doc.data().profileId);
-// const findChoreHistoryId = querySnapshot.docs.map((doc) => doc.data().id);
+export const getChoreHistoryFromDbByChores = createAsyncThunk<
+  ChoreHistory[],
+  Chore[],
+  { rejectValue: string }
+>('household/getchorehistorybychoreid', async (chores, thunkApi) => {
+  try {
+    if (chores.length > 0) {
+      const choreIds = chores.map((c) => c.id);
+      const q = query(collection(db, 'ChoreHistory'), where('choreId', 'in', choreIds));
+      const querySnapshot = await getDocs(q);
+      const result = querySnapshot.docs.map((doc) => doc.data() as ChoreHistory);
+      return result;
+    } else return [];
+  } catch (error) {
+    console.error(error);
+    if (error instanceof FirebaseError) {
+      console.log(error.message);
+      return thunkApi.rejectWithValue('Databasfel, kunde inte hämta historiken.');
+    }
+    return thunkApi.rejectWithValue('Något gick snett, vänligen kontakta support!');
+  }
+});
 
 const choreHistorySlice = createSlice({
   name: 'choreHistory',
@@ -107,58 +96,44 @@ const choreHistorySlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(addChoreHistoryToDb.pending, (state) => {
       state.isLoading = true;
-      console.log('pending');
+      console.log('addChoreHistoryToDb pending');
     });
     builder.addCase(addChoreHistoryToDb.fulfilled, (state, action) => {
       state.isLoading = false;
       state.choresHistory.push(action.payload);
-      console.log('fulfill');
+      console.log('addChoreHistoryToDb fulfilled');
     });
     builder.addCase(addChoreHistoryToDb.rejected, (state) => {
       state.isLoading = false;
-      console.log('rejected');
+      console.log('addChoreHistoryToDb rejected');
     });
 
     builder.addCase(getChoreHistoryFromDbByProfileIds.pending, (state) => {
       state.isLoading = true;
-      console.log('pending');
+      console.log('getChoreHistoryFromDbByProfileIds pending');
     });
     builder.addCase(getChoreHistoryFromDbByProfileIds.fulfilled, (state, action) => {
       state.isLoading = false;
       state.choresHistory = action.payload;
-      console.log('fulfill');
+      console.log('getChoreHistoryFromDbByProfileIds fulfilled');
     });
     builder.addCase(getChoreHistoryFromDbByProfileIds.rejected, (state) => {
       state.isLoading = false;
-      console.log('rejected');
+      console.log('getChoreHistoryFromDbByProfileIds rejected');
     });
 
     builder.addCase(getChoreHistoryFromDbByChores.pending, (state) => {
       state.isLoading = true;
-      console.log('pending');
+      console.log('getChoreHistoryFromDbByChores pending');
     });
     builder.addCase(getChoreHistoryFromDbByChores.fulfilled, (state, action) => {
       state.isLoading = false;
       state.choresHistory = action.payload;
-      console.log('fulfill');
+      console.log('getChoreHistoryFromDbByChores fulfill');
     });
     builder.addCase(getChoreHistoryFromDbByChores.rejected, (state) => {
       state.isLoading = false;
-      console.log('rejected');
-    });
-
-    //getDateWhenLatestDoneChoreHistoryWithChoreId
-    builder.addCase(getDateWhenLatestDoneChoreHistoryWithChoreId.pending, (state) => {
-      state.isLoading = true;
-      console.log('pending : getDateWhenLatestDoneChoreHistoryWithChoreId');
-    });
-    builder.addCase(getDateWhenLatestDoneChoreHistoryWithChoreId.fulfilled, (state) => {
-      state.isLoading = false;
-      console.log('fulfill : getDateWhenLatestDoneChoreHistoryWithChoreId');
-    });
-    builder.addCase(getDateWhenLatestDoneChoreHistoryWithChoreId.rejected, (state) => {
-      state.isLoading = false;
-      console.log('rejected : getDateWhenLatestDoneChoreHistoryWithChoreId');
+      console.log('getChoreHistoryFromDbByChores rejected');
     });
   },
 });
@@ -178,10 +153,6 @@ export const selectHistoryByPeriod =
     );
     const choresInHousehold = state.chore.chores;
     const choreHistories = state.choreHistory.choresHistory.filter((ch) => ch.profileId !== 'null');
-
-    console.log('||||||||||||||||||||| choreHistories |||||||||||||||||||||');
-    console.log(choreHistories);
-
     const choreHistoryForCurrentWeek = filterCurrentWeek(choreHistories);
 
     profilesInHousehold.forEach((pro) => {
