a4a47147fcc45b6d22fd0b48f4da1735d4e93aa9
LuminTompa <lumincob@gmail.com>
2022-11-04 14:59:18 +0100

    fix legend styling

--- src/Screens/StatisticsScreen.tsx
+++ src/Screens/StatisticsScreen.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
 import { Text } from 'react-native-paper';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import ChorePieChart from '../Components/ChorePieChart';
+import TotalChorePieChart from '../Components/TotalChorePieChart';
 import { selectHistoryByPeriod } from '../Store/choreHistorySlice';
 import { useAppSelector } from '../Store/store';
 
@@ -22,29 +22,22 @@ export interface ChorePieData {
 export default function StatisticsScreen() {
   // const { totalData, everyPieData } = useAppSelector(selectHistoryByPeriod(route.params.period));
   const { totalData, everyPieData } = useAppSelector(selectHistoryByPeriod('currentWeek'));
-  const totalDataToDecimal = totalData.map((data) => {
-    return {
-      ...data,
-      contribution: data.contribution / 10,
-    };
-  });
+
   return (
     <ScrollView>
-      <SafeAreaView>
-        <View style={styles.container}>
-          <View style={{ paddingLeft: 70 }}>
-            <ChorePieChart width={550} height={330} hasLegend data={totalData} />
-          </View>
-          <View style={styles.smallPiechartContainer}>
-            {everyPieData.map((data) => (
-              <View key={data.choreTitle} style={styles.smallPiechart}>
-                <Text>{data.choreTitle}</Text>
-                <ChorePieChart width={140} height={100} hasLegend={false} data={data.pieData} />
-              </View>
-            ))}
-          </View>
+      <View style={styles.container}>
+        <View style={styles.totalChartContainer}>
+          <TotalChorePieChart width={500} height={250} hasLegend={true} data={totalData} />
         </View>
-      </SafeAreaView>
+        <View style={styles.smallPiechartContainer}>
+          {everyPieData.map((data) => (
+            <View key={data.choreTitle} style={styles.smallPiechart}>
+              <ChorePieChart width={140} height={100} hasLegend={false} data={data.pieData} />
+              <Text>{data.choreTitle}</Text>
+            </View>
+          ))}
+        </View>
+      </View>
     </ScrollView>
   );
 }
@@ -54,9 +47,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1,
   },
+  totalChartContainer: {
+    paddingBottom: 30,
+  },
   smallPiechart: {
     alignItems: 'center',
     maxWidth: 100,
+    padding: 10,
   },
   smallPiechartContainer: {
     flexDirection: 'row',
